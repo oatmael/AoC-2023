a996eb6003520906caf9f3d3ed0ccd24993985c8
Jaron Harrison <jaronharrison1605@gmail.com>
2023-12-02 23:18:25 +1030

    Apply gitignore

--- day1/target/debug/day1.d
+++
@@ -1,1 +0,0 @@
-E:\Projects\AoC-2023\day1\target\debug\day1.exe: E:\Projects\AoC-2023\day1\src\main.rs
--- day1/target/debug/deps/day1.d
+++
@@ -1,5 +0,0 @@
-E:\Projects\AoC-2023\day1\target\debug\deps\day1.exe: src\main.rs
-
-E:\Projects\AoC-2023\day1\target\debug\deps\day1.d: src\main.rs
-
-src\main.rs:
